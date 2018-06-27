a0ff165448c58e92f7db7995e660ae82f74467a0
Austin Barr and Hisato Kawaminami <austin@>
2018-06-27 10:38:01 -0700

    testing testing

--- TodoList.Tests/ModelTests/ItemTest.cs
+++ TodoList.Tests/ModelTests/ItemTest.cs
@@ -1,25 +1,86 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
+
   {
 
-  [TestMethod]
-  public void GetDescription_SetDescription_String()
-  {
-    //Arrange
-    string description = "Walk the dog.";
-    Item newItem = new Item(description);
-    description = "Do the dishes";
-    //Act
-    newItem.SetDescription(description);
-    string result = newItem.GetDescription();
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Make dinner.";
+      Item newItem = new Item(description);
 
-    //Assert
-    Assert.AreEqual(description, result);
+      //Act
+      string result = newItem.GetDescription();
+
+      //Assert
+      Assert.AreEqual(description, result);
+      // newItem.Dispose();
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      description = "Do the dishes";
+      //Act
+      newItem.SetDescription(description);
+      string result = newItem.GetDescription();
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void Save_ReturnsItem_Item()
+    {
+      //Arrange
+      string description = "Walk the Dog.";
+      Item newItem = new Item(description);
+      newItem.Save();
+
+      //Act
+      List<Item> instances = Item.GetAll();
+      Item savedItem = instances[0];
+      Console.WriteLine(savedItem.GetDescription());
+
+      //Assert
+      Assert.AreEqual(newItem, savedItem);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      newItem1.Save();
+      Item newItem2 = new Item(description02);
+      newItem2.Save();
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+      foreach (Item thisItem in result)
+      {
+        Console.WriteLine("Output: " + thisItem.GetDescription());
+      }
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+    public void Dispose()
+  {
+    Item.ClearAll();
   }
   }
 }
