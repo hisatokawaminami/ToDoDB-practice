84a159c89eba81df6398a094b0f4a3608330b2fe
Austin Barr and Hisato Kawaminami <austin@>
2018-06-27 13:57:48 -0700

    work

--- TodoList/models/item.cs
+++ TodoList/models/item.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
+using System;
 namespace ToDoList.Models
 {
   public class Item
   {
     private string _description;
+    private static List<Item> _instances = new List<Item> {};
+
     public Item (string description)
     {
-       _description = description;
+      _description = description;
     }
     public string GetDescription()
     {
@@ -14,6 +18,47 @@ namespace ToDoList.Models
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
+    }
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to the TO DO LIST");
+      Console.WriteLine("Would you like to add an item to your list or view your list?(Add/View)");
+
+      string addView = Console.ReadLine().ToUpper();
+      if(addView == "ADD")
+      {
+        Console.WriteLine("Enter a to-do...");
+        Item userItem = new Item(Console.ReadLine());
+        userItem.Save();
+        Console.WriteLine (userItem.GetDescription() + " has been added to your list");
+        Main();
+      }
+      else
+      {
+        List<Item> result = Item.GetAll();
+        foreach (Item thisItem in result)
+        {
+          Console.WriteLine(thisItem.GetDescription());
+        }
+        // Console.WriteLine("Here is yout to-do's: " + result);
+        // Console.WriteLine(userItem.GetAll());
+      }
     }
   }
 }
