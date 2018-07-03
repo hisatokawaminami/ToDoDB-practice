4b2f7ee8baf16be97bd2bf7b25117783c540387e
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-07-03 16:15:04 -0700

    Add additional code to create links for each list item based off of a unique id.

--- TodoList/Controllers/ItemsController.cs
+++ TodoList/Controllers/ItemsController.cs
@@ -6,29 +6,31 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-
     [HttpGet("/items")]
     public ActionResult Index()
     {
-      Item newItem = new Item(Request.Query["new-item"]);
-      newItem.Save();
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
+        List<Item> allItems = Item.GetAll();
+        return View(allItems);
     }
 
-    [HttpGet("/list")]
-    public ActionResult ShowList()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View("Index", allItems);
-    }
-
-    [HttpGet("/new")]
+    [HttpGet("/items/new")]
     public ActionResult CreateForm()
     {
-      return View();
+        return View();
     }
 
-
+    [HttpPost("/items")]
+    public ActionResult Create()
+    {
+        Item newItem = new Item(Request.Form["new-item"]);
+        List<Item> allItems = Item.GetAll();
+        return View("Index", allItems);
+    }
+    [HttpGet("/items/{id}")]
+    public ActionResult Details(int id)
+    {
+        Item item = Item.Find(id);
+        return View(item);
+    }
   }
 }
