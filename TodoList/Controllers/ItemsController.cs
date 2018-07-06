0f20162c90d259c5c8c4b75af03e84022ae2b46c
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-07-05 19:38:51 -0700

    add category

--- TodoList/Controllers/ItemsController.cs
+++ TodoList/Controllers/ItemsController.cs
@@ -6,31 +6,48 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/items")]
-    public ActionResult Index()
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    // {
+    //     List<Item> allItems = Item.GetAll();
+    //     return View(allItems);
+    // }
+    //
+    // [HttpGet("/items/new")]
+    // public ActionResult CreateForm()
+    // {
+    //     return View();
+    // }
+    //
+    // // [HttpPost("/items")]
+    // // public ActionResult Create()
+    // // {
+    // //     Item newItem = new Item(Request.Form["new-item"]);
+    // //     List<Item> allItems = Item.GetAll();
+    // //     return RedirectToAction("Index", allItems);
+    // // }
+    // [HttpGet("/items/{id}")]
+    // public ActionResult Details(int id)
+    // {
+    //     Item item = Item.Find(id);
+    //     return View(item);
+    // }
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult CreateForm(int categoryId)
     {
-        List<Item> allItems = Item.GetAll();
-        return View(allItems);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+    Category category = Category.Find(categoryId);
+    return View(category);
     }
-
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Details(int categoryId, int itemId)
     {
-        return View();
-    }
-
-    [HttpPost("/items")]
-    public ActionResult Create()
-    {
-        Item newItem = new Item(Request.Form["new-item"]);
-        List<Item> allItems = Item.GetAll();
-        return View("Index", allItems);
-    }
-    [HttpGet("/items/{id}")]
-    public ActionResult Details(int id)
-    {
-        Item item = Item.Find(id);
-        return View(item);
+      Item item = Item.Find(itemId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category category = Category.Find(categoryId);
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(item);
     }
   }
 }
